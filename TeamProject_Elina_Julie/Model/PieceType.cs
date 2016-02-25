8720f37e7e0ee69af6bcb01762e15a2eb537c6ce
Elina <sonarsinmiedo@gmail.com>
2016-02-25 19:21:11 +0400

    Board Template

---
+++ TeamProject_Elina_Julie/Model/PieceType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamProject_Elina_Julie.Model
+{
+    public enum PieceType
+    {
+        Pawn,
+        Rook,
+        Knight,
+        Bishop,
+        Queen,
+        King
+    }
+}
+ //need to extend each piece with own move methods
+ //probably in separated classes
---
+++ TeamProject_Elina_Julie/ViewModel/ChessPiece.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Windows;
+using TeamProject_Elina_Julie.Model;
+
+namespace TeamProject_Elina_Julie
+{
+    class ChessPiece: ViewModelBase
+    {
+        private Point _Pos;
+        public Point Pos
+        {
+            get { return this._Pos; }
+            set { this._Pos = value; RaisePropertyChanged(() => this.Pos); }
+        }
+
+        private PieceType _Type;
+        public PieceType Type
+        {
+            get { return this._Type; }
+            set { this._Type = value; RaisePropertyChanged(() => this.Type); }
+        }
+
+        private Player _Player;
+        public Player Player
+        {
+            get { return this._Player; }
+            set { this._Player = value; RaisePropertyChanged(() => this.Player); }
+        }
+    }
+}
